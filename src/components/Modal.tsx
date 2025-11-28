e896d830dfd18073ce44addeb9c4e3f0dc9354fe
appesteticaprohub <appesteticaprohub@gmail.com>
2025-11-28 11:55:10 -0500

    Corrección deploy en src/components/Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,7 +11,7 @@ interface ModalProps {
 export default function Modal({ isOpen, onClose, children, showLockIcon = true }: ModalProps) {
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-50" onClose={() => {}}>
+      <Dialog as="div" className="relative z-50" onClose={onClose}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
